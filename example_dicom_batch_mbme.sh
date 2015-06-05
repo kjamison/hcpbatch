d50eff18bb689fd43a6a7eee63fdd96baac2910f
Keith Jamison <kjamison@umn.edu>
2015-06-05 14:38:57 -0500

    Update example dicom_init

--- example_dicom_batch_mbme.sh
+++ example_dicom_batch_mbme.sh
@@ -1,13 +1,10 @@
-cmd="bash Default/batch_dicom_init.sh mbme ~/range3hcp/20150427-ST001-jamison_mbme1/ subjEK "
+cmd="bash batch_dicom_init.sh mbme ~/range3hcp/20150427-ST001-MBMETEST/ SUBJ01 "
 
-#eval "$cmd rfMRI_REST1_2mm_PA                  BOLD=12 SE=13,14"
-#eval "$cmd rfMRI_REST1_MB4R2ME3_2.5mm6_8pf_PA  BOLD=16 SE=13,14"
-#eval "$cmd rfMRI_REST1_2.5mm_PA                BOLD=18 SE=13,14"
+SE="34,35"
 
-eval "$cmd REST2_2.5mm_MB4R2ME3_6_8pf_PA  BOLD=29 SE=34,35"
-eval "$cmd REST2_2.5mm_PA 	       BOLD=31 SE=34,35"
-eval "$cmd REST2_2.0mm_PA 	       BOLD=33 SE=34,35"
-eval "$cmd REST2_3.0mm_PA 	       BOLD=37 SE=34,35"
-eval "$cmd REST2_3.0mm_MB4R2ME3_7_8pf_PA    BOLD=39 SE=34,35"
-
+eval "$cmd REST2_2.5mm_MB4R2ME3_6_8pf_PA   BOLD=29 SE=$SE"
+eval "$cmd REST2_2.5mm_PA                  BOLD=31 SE=$SE"
+eval "$cmd REST2_2.0mm_PA                  BOLD=33 SE=$SE"
+eval "$cmd REST2_3.0mm_PA                  BOLD=37 SE=$SE"
+eval "$cmd REST2_3.0mm_MB4R2ME3_7_8pf_PA   BOLD=39 SE=$SE"
 
